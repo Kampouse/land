49b00e3df5d96c7503e0b37714dd0452bbc3ea89
jean-philippe martel <j-p@jean-philippes-MacBook-Air.local>
2024-04-28 17:24:24 -0400

    severless being rude

--- src/routes/og-image/index.ts
+++ src/routes/og-image/index.ts
@@ -1,44 +1,35 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
-import type { PlatformCloudflarePages } from "@builder.io/qwik-city/middleware/cloudflare-pages";
-import { fetchFont, ImageResponse, html } from "og-img";
-export const onGet: RequestHandler<PlatformCloudflarePages> = async ({
-  send,
-  url,
-}) => {
-  console.log(url);
-  fetchFont(`${url.origin}/fonts/aria-violet.ttf`)
-    .then((data) => {
-      send(
-        new ImageResponse(
-          html`
-            <div tw="text-4xl text-green-700" style="background-color: tan">
-              Hello, world!
-            </div>
-          `,
+import { ImageResponse, fetchFont, html } from "og-img";
+export const onGet: RequestHandler = async ({ send, url }) => {
+  let font = null;
+  try {
+    font = await fetchFont(`${url.origin}/fonts/aria-violet.ttf`);
+  } catch (e) {
+    console.error(e);
+    send(new Response("Failed to fetch font", { status: 500 }));
+    return;
+  }
+
+  send(
+    new ImageResponse(
+      html`
+        <div tw="text-4xl text-green-700" style="background-color: tan">
+          Hello, world!
+        </div>
+      `,
+      {
+        width: 300,
+        height: 300,
+        fonts: [
           {
-            width: 300,
-            height: 300,
-            fonts: [
-              {
-                name: "Roboto",
-                data: data,
-                weight: 400,
-                style: "normal",
-              },
-            ],
+            name: "Roboto",
+            // Use `fs` (Node.js only) or `fetch` to read font file
+            data: font,
+            weight: 400,
+            style: "normal",
           },
-        ),
-      );
-    })
-    .catch((e) => {
-      //create a response showing that it could not fetch the font
-      send(
-        new ImageResponse(
-          html`<div tw="text-4xl text-green-700" style="background-color: tan">
-            Hello, world!
-          </div>`,
-          { width: 300, height: 300, fonts: [] },
-        ),
-      );
-    });
+        ],
+      },
+    ),
+  );
 };
