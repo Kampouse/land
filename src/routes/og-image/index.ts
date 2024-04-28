497466a43121f0f643717964b3b2ee2ef93fd436
jean-philippe martel <j-p@jean-philippes-MacBook-Air.local>
2024-04-28 14:54:54 -0400

    testing

--- src/routes/og-image/index.ts
+++ src/routes/og-image/index.ts
@@ -4,19 +4,17 @@ import { fetchFont, ImageResponse, html } from "og-img";
 export const onGet: RequestHandler = async (req) => {
   req.send(
     new ImageResponse(
-      // Use Tailwind CSS or style attribute
       html`
         <div tw="text-4xl  " style="background-color: red">
           jemartel / porfolio
         </div>
       `,
       {
-        width: 1200,
-        height: 600,
+        width: 300,
+        height: 300,
         fonts: [
           {
             name: "Roboto",
-            // Use `fs` (Node.js only) or `fetch` to read font file
             data: await fetchFont("https://jemartel.dev/fonts/aria-violet.ttf"),
             weight: 400,
             style: "normal",
