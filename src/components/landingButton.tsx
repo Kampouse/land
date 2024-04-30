d81901e8baff5ff763b73fdbbd49021e13a75508
jean-philippe <jp@zelexio.com>
2024-04-29 23:13:58 -0400

    fixed the button weirdness on mobile

--- src/components/landingButton.tsx
+++ src/components/landingButton.tsx
@@ -7,7 +7,7 @@ import { Slot, component$ } from "@builder.io/qwik";
 export default component$<props>((props) => {
   return (
     <a
-      class="border  border-red-50 self-center   m-2 p-2 lg:p-5     w-20 lg:w-fit rounded-2xl hover:bg-[#1a74ba] text-white bg-clip-text text-transparent "
+      class="border  border-red-50 self-center   m-1 p-2 lg:p-5     w-20 lg:w-fit rounded-2xl hover:bg-[#1a74ba] text-white bg-clip-text text-transparent "
       target="_blank"
       href={props.link}
     >
