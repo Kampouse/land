16a135bff41cfbfb49c45df95fdf45d8b1ebc79b
jean-philippe <jp@zelexio.com>
2024-06-30 14:15:40 -0400

    good stuff

--- src/components/blogheader.tsx
+++ src/components/blogheader.tsx
@@ -2,8 +2,8 @@ import { component$, Slot } from "@builder.io/qwik";
 
 export const Heading = component$(() => {
   return (
-    <div class="lg:py-16  py-12   gap-20 text-left bg-[#221539]  rounded-xl  mb-6">
-      <h1 class="p-0 m-0 ml-5 font-mono  text-8xl font-normal tracking-tighter [word-spacing:0.3em] first-letter:font-bold first-letter:text-[#a52227] first-letter:text-[1.5em] text-white border-0">
+    <div class="     gap-20 text-left   rounded-xl  mb-6">
+      <h1 class=" font-mono  text-8xl font-normal tracking-tighter [word-spacing:0.3em] first-letter:font-bold first-letter:text-[#a52227] first-letter:text-[1.5em] text-white border-0">
         <Slot />
       </h1>
     </div>
--- src/routes/blog/layout.tsx
+++ src/routes/blog/layout.tsx
@@ -4,8 +4,8 @@ export default component$(() => {
   useStyles$(styles);
   return (
     <div class=" flex bg-hero-pattern bg-cotain min-h-screen bg-cover bg-repeat-y ">
-      <div class="flex    my-4 mx-2 lg:mx-8   rounded-xl   w-full py-6        bg-[#291D53] ">
-        <article class="post mx-4  my-3  md:mx-24 lg:mx-40">
+      <div class="flex    m-2 lg:mx-8   rounded-xl   w-full py-6        bg-[#291D53] ">
+        <article class="post mx-4  my-3  md:mx-24 lg:mx-40 max">
           <Slot />
         </article>
       </div>
