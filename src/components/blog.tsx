f4239b9a03892a76ea271260b332c03a6e8bc23c
jean-philippe <jp@zelexio.com>
2024-07-09 21:51:25 -0400

    update somewhat

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -23,31 +23,31 @@ export default component$(() => {
     <div class=" flex flex-col">
       <div class=" text-center place-self-center  text-white  ">
         <hr class=" h-px my-2  bg-gray-800 border-0 flex w-full "></hr>
-        <div class="  pb-2 ">
+        <div class="  pb-2 text-2xl font-medium ">
           Blogs entries <lucid.ArrowDownIcon class="inline ml-2" size={20} />
         </div>
         <ul
-          class="  list-decimal flex flex-col  mb-4"
+          class="flex flex-col mx-2 mb-4"
           about="list of blog entries"
           aria-label="list of blog entries"
         >
           {blogEntries.map((blog: BlogEntry) => {
             return (
               <li
-                class=" text-white text-left m-2 cursor-pointer   border-opacity-40  border-b-2  border-gray-800 border rounded-lg transition ease-linear hover:bg-opacity-75  hover:scale-105 hover:shadow-lg"
+                class=" text-white text-left m-2 cursor-pointer   border-opacity-40  border-b-2  py-4 border-[#e6284b] border rounded-lg transition ease-linear hover:bg-opacity-75  hover:scale-105 hover:shadow-lg"
                 onClick$={() => navi(blog.link)}
                 key={blog.title}
-                aria-label="blog entry"
+                aria-label="blog entrie"
               >
                 <Link
-                  class="text-left  text-xl  ml-5"
+                  class="text-left  text-xl px-4"
                   href={blog.link}
                   aria-label="link to blog"
                   about="link to blog"
                 >
                   {blog.title}
                 </Link>
-                <h3 class=" border-opacity-40  text-sm   text-center ml-5 text-gray-400">
+                <h3 class=" border-opacity-40  text-sm px-4   text-center  text-gray-400">
                   {" "}
                   {blog.description}
                 </h3>
--- src/components/landingButton.tsx
+++ src/components/landingButton.tsx
@@ -7,7 +7,7 @@ import { Slot, component$ } from "@builder.io/qwik";
 export default component$<props>((props) => {
   return (
     <a
-      class=" flex border  border-red-50 justify-center hover:scale-110 transition      m-1 p-2 lg:p-5 content-center    w-full lg:w-fit rounded-2xl hover:bg-[#1a74ba] text-white bg-clip-text text-transparent "
+      class=" flex border  border-red-50 justify-center  transition hover:text-[#e6284b] font-medium      m-1 p-2 lg:p-5 content-center    w-full lg:w-fit rounded-2xl hover:bg-[#1a74ba] text-white bg-clip-text text-transparent "
       target="_blank"
       href={props.link}
     >
