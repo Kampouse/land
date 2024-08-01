6a4c5f8f2a70663676fd4d9a581693dfa41b0bc7
jean-philippe <jp@zelexio.com>
2024-08-01 13:19:48 -0400

    add  posthog

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,9 @@
-import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
+import { component$, Slot, useStyles$, useTask$, useVisibleTask$ } from "@builder.io/qwik";
+import { routeLoader$, } from "@builder.io/qwik-city";
+import { isServer } from '@builder.io/qwik/build';
 import type { RequestHandler } from "@builder.io/qwik-city";
 import styles from "./styles.css?inline";
+import post from "posthog-js"
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.builder.io/docs/caching/
@@ -20,6 +22,25 @@ export const useServerTimeLoader = routeLoader$(() => {
 });
 
 export default component$(() => {
+
+  //eslint-disable-next-line
+  useVisibleTask$(() => {
+    if (document.location.host.includes("jemartel.dev")) {
+      post.init(
+        'phc_O45dLj5YhxXeIEbjmqi8sw6p2ooKUosN02i2mgXTlVf',
+        {
+          api_host: "https://us.i.posthog.com",
+          capture_heatmaps: true,
+          capture_pageview: true,
+          capture_pageleave: true,
+          person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
+          capture_performance: true,
+          disable_external_dependency_loading: false, // Optional - will ensure we never try to load extensions lazily
+
+        }
+      )
+    }
+  });
   useStyles$(styles);
   return (
     <>
